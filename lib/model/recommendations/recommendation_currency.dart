0eee8a651c0f5d349683e85197c7876a252c609a
FelipeOFF <felipe-not@hotmail.com>
2025-08-06 01:36:14 -0300

    Refactor `RecommendationCurrency` into its own file and update imports
    - Extract `RecommendationCurrency` from `recommendation_req.dart` to `recommendation_currency.dart` for better separation of concerns.
    - Update `recommendation_req.dart` to import the new `recommendation_currency.dart`.

---
+++ lib/model/recommendations/recommendation_currency.dart
@@ -0,0 +1,30 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+enum RecommendationCurrency {
+  // FIAT currencies
+  @JsonValue('BRL')
+  brl('BRL'),
+  @JsonValue('COP')
+  cop('COP'),
+  @JsonValue('PEN')
+  pen('PEN'),
+  @JsonValue('VES')
+  ves('VES'),
+
+  // Crypto currencies
+  @JsonValue('TATUM-TRON-USDT')
+  tatumTronUSDT('TATUM-TRON-USDT'),
+  @JsonValue('TATUM-TRON-USDC')
+  tatumTronUSDC('TATUM-TRON-USDC');
+
+  const RecommendationCurrency(this.value);
+
+  final String value;
+
+  // Helper methods para identificar o tipo
+  List<RecommendationCurrency> get fiat => [brl, cop, pen, ves];
+  List<RecommendationCurrency> get crypto => [tatumTronUSDT, tatumTronUSDC];
+
+  bool get isFiat => fiat.contains(this);
+  bool get isCrypto => crypto.contains(this);
+}
--- lib/model/recommendations/req/recommendation_req.dart
+++ lib/model/recommendations/req/recommendation_req.dart
@@ -1,36 +1,8 @@
+import 'package:coding_interview_frontend/model/recommendations/recommendation_currency.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'recommendation_req.freezed.dart';
 part 'recommendation_req.g.dart';
-
-enum RecommendationCurrency {
-  // FIAT currencies
-  @JsonValue('BRL')
-  brl('BRL'),
-  @JsonValue('COP')
-  cop('COP'),
-  @JsonValue('PEN')
-  pen('PEN'),
-  @JsonValue('VES')
-  ves('VES'),
-
-  // Crypto currencies
-  @JsonValue('TATUM-TRON-USDT')
-  tatumTronUSDT('TATUM-TRON-USDT'),
-  @JsonValue('TATUM-TRON-USDC')
-  tatumTronUSDC('TATUM-TRON-USDC');
-
-  const RecommendationCurrency(this.value);
-
-  final String value;
-
-  // Helper methods para identificar o tipo
-  List<RecommendationCurrency> get fiat => [brl, cop, pen, ves];
-  List<RecommendationCurrency> get crypto => [tatumTronUSDT, tatumTronUSDC];
-
-  bool get isFiat => fiat.contains(this);
-  bool get isCrypto => crypto.contains(this);
-}
 
 @freezed
 abstract class RecommendationReq with _$RecommendationReq {
