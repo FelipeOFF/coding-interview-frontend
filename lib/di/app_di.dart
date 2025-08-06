0988b0435a4f00df8d74a201509109283365e4d9
FelipeOFF <felipe-not@hotmail.com>
2025-08-05 22:50:32 -0300

    Add dependency injection setup using `GetIt` with bindings abstraction

---
+++ lib/di/app_di.dart
@@ -0,0 +1,23 @@
+import 'package:coding_interview_frontend/configurations/di/app_config_binding.dart';
+import 'package:coding_interview_frontend/di/abstract_binding.dart';
+import 'package:get_it/get_it.dart';
+
+class AppDI {
+  const AppDI._();
+
+  static final GetIt it = GetIt.instance;
+
+  static final List<AbstractBinding> _listOfBasicInfrastructure = [
+    AppConfigBinding(),
+  ];
+
+  static final List<AbstractBinding> _getListOfBiding = [
+    ..._listOfBasicInfrastructure,
+  ];
+
+  static Future<void> setupDI() async {
+    for (final element in _getListOfBiding) {
+      await element.binding(it);
+    }
+  }
+}
